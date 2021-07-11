0514fda656cad915fe95c28c95c52c64e70b72a4
VivekG11 <vivekvk2903@gmail.com>
2021-07-12 00:00:20 +0530

    [Inserting elemenst to stack]

---
+++ StackAndQueue/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StackAndQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stack Insert operation.......");
+            StackFunction stack = new StackFunction();
+            stack.push(70);
+            stack.push(30);
+            stack.push(56);
+            stack.Display();
+        }
+    }
+}
---
+++ StackAndQueue/StackFunction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueue
+{
+    class StackFunction
+    {
+        public Node top;
+        public void push(int data)
+        {
+            Node node = new Node(data);
+            if(this.top == null)
+            {
+                this.top = node;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            Console.WriteLine(node.data+" is added to stack.");
+            
+        }
+        public void Display()
+        {
+            int count = 0;
+            Node temp = this.top;
+            if(temp == null)
+            {
+                Console.WriteLine("Stack is Empty......");
+            }
+            Console.WriteLine("Elements in he Stack are ......");
+            while (temp!= null)
+            {
+                Console.Write(temp.data+",");
+                temp = temp.next;
+                count++;
+            }
+            Console.WriteLine("\nTotal Elements are  :"+count);
+
+        }
+    }
+}
