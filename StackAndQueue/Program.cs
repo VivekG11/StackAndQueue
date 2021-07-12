0d9c5ac5fdda82819d121889f6c3188fd329c5e0
VivekG11 <vivekvk2903@gmail.com>
2021-07-12 20:14:21 +0530

    [Enqueue and dequeue operation..]

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -8,6 +8,9 @@ namespace StackAndQueue
         {
             Console.WriteLine("Stack Insert operation.......");
             QueueFunction queue = new QueueFunction();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
             queue.Dequeue();
             queue.Dequeue();
             queue.Dequeue();
